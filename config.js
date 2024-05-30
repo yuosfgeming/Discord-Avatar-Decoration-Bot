57069283414634039950aea0c244d18b6e352b15
AyoubDV <167919901+AyoubDV@users.noreply.github.com>
2024-05-30 09:16:51 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ Copyright (c) 2024 Wick Studio
 */
 
 module.exports = {
-    guildId: "guild", // your server id
-    token: "token", // your bot token
+    guildId: "1239215615705612368", // your server id
+    token: "MTIzOTIxNTk0MDE3Mjc3OTUyMA.GAe0Jq.vGIW9bttMBrW6VxQzEboyJfZA0UqLBcya05Bls", // your bot token
   };
   
